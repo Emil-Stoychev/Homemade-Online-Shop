6b78995f3b87b6cf1c77222024c7bbfffd12ef7b
Емил Стойчев <estallinone@gmail.com>
2022-11-26 11:37:14 +0200

    Add main module with general components inside
    Like home, catalog, about, create

--- my-app/src/app/main/main.module.ts
+++ my-app/src/app/main/main.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CreateComponent } from './create/create.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    CatalogComponent,
+    CreateComponent,
+    AboutComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomeComponent,
+    CatalogComponent,
+    CreateComponent,
+    AboutComponent
   ]
 })
 export class MainModule { }
