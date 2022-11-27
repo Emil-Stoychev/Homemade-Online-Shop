8a132006d382e273c01237c992da10bac85939bb
Емил Стойчев <estallinone@gmail.com>
2022-11-27 07:49:46 +0200

    Add details, profile, edit components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module'
+import { MainModule } from './main/main.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    MainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
