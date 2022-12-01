bd8b3542ed220f4f3c9650418d90e3dab872100f
Емил Стойчев <estallinone@gmail.com>
2022-12-01 19:33:26 +0200

    Add http client to app

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
-import { AboutComponent } from './main/about/about.component';
 import { CatalogComponent } from './main/catalog/catalog.component';
 import { CreateComponent } from './main/create/create.component';
 import { DetailsComponent } from './main/details/details.component';
@@ -11,22 +10,25 @@ import { HomeComponent } from './main/home/home.component';
 import { ProfileComponent } from './main/profile/profile.component';
 import { LoginComponent } from './main/user/login/login.component';
 import { RegisterComponent } from './main/user/register/register.component';
+import { OwnProductsComponent } from './main/ownProducts/ownProducts.component';
+import { LikedProductsComponent } from './main/likedProducts/likedProducts.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full'},
-  { path: 'catalog', component: CatalogComponent},
-  { path: 'create', component: CreateComponent},
-  { path: 'profile', component: ProfileComponent},
-  { path: 'catalog/details/:id', component: DetailsComponent},
-  { path: 'catalog/edit/:id', component: EditComponent},
-  { path: 'about', component: AboutComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: '**', component: PageNotFoundComponent},
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'catalog/details/:id', component: DetailsComponent },
+  { path: 'catalog/edit/:id', component: EditComponent },
+  { path: 'ownProducts', component: OwnProductsComponent },
+  { path: 'likedProducts', component: LikedProductsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { MainModule } from './main/main.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    MainModule
+    MainModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
