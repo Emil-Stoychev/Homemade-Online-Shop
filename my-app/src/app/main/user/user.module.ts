9ab606e1f45365159ce350106703ef6f9e379cfe
Емил Стойчев <estallinone@gmail.com>
2022-12-02 18:09:46 +0200

    Add forms module

--- my-app/src/app/main/user/user.module.ts
+++ my-app/src/app/main/user/user.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -11,7 +14,12 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent
   ]
 })
 export class UserModule { }
