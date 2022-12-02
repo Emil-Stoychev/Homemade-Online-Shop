65586989b1b9ec30cead185cf6f348a475de4b4a
Емил Стойчев <estallinone@gmail.com>
2022-12-02 18:11:12 +0200

    Edit some exports

--- my-app/src/app/main/main.module.ts
+++ my-app/src/app/main/main.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CreateComponent } from './create/create.component';
 import { OwnProductsComponent } from './ownProducts/ownProducts.component';
+import { LikedProductsComponent } from './likedProducts/likedProducts.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DetailsComponent } from './details/details.component';
+import { EditComponent } from './edit/edit.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +17,21 @@ import { OwnProductsComponent } from './ownProducts/ownProducts.component';
     CatalogComponent,
     CreateComponent,
     OwnProductsComponent,
+    LikedProductsComponent,
+    ProfileComponent,
+    DetailsComponent,
+    EditComponent,
   ],
-  imports: [CommonModule],
-  exports: [HomeComponent, CatalogComponent, CreateComponent, OwnProductsComponent],
+  imports: [CommonModule, UserModule],
+  exports: [
+    HomeComponent,
+    CatalogComponent,
+    CreateComponent,
+    OwnProductsComponent,
+    LikedProductsComponent,
+    ProfileComponent,
+    DetailsComponent,
+    EditComponent
+  ],
 })
 export class MainModule {}
