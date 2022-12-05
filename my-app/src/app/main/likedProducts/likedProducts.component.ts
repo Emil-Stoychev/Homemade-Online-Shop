16c95685be6cd02e43555207619e5877343af88b
Емил Стойчев <estallinone@gmail.com>
2022-12-05 15:32:00 +0200

    Add message to own and like products when products is 0

--- my-app/src/app/main/likedProducts/likedProducts.component.ts
+++ my-app/src/app/main/likedProducts/likedProducts.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
+import { CatalogService } from 'src/app/services/catalog/catalog.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-likedProducts',
   templateUrl: './likedProducts.component.html',
-  styleUrls: ['./likedProducts.component.css']
+  styleUrls: ['./likedProducts.component.css'],
 })
 export class LikedProductsComponent implements OnInit {
+  public products: any = [];
+  public errors: any = [];
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private appComponent: AppComponent,
+    private catalogService: CatalogService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.userService
+      .getProfile(this.appComponent.sessionStorage)
+      .subscribe((data: any) => {
+        this.catalogService
+          .getLikedProducts(data._id)
+          .subscribe((data: any) => {
+            if (data?.message) {
+              this.errors = data
+            } else {
+              this.products = data;
+            }
+          });
+      });
   }
 
+  onSelect(id: string) {
+    this.router.navigate(['/catalog/details/', id]);
+  }
 }
--- my-app/src/app/main/ownProducts/ownProducts.component.ts
+++ my-app/src/app/main/ownProducts/ownProducts.component.ts
@@ -1,17 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
 import { CatalogService } from 'src/app/services/catalog/catalog.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-ownProducts',
   templateUrl: './ownProducts.component.html',
-  styleUrls: ['./ownProducts.component.css']
+  styleUrls: ['./ownProducts.component.css'],
 })
 export class OwnProductsComponent implements OnInit {
+  public products: any = [];
+  public errors: any = [];
 
-  constructor(private catalogService: CatalogService) { }
+  constructor(
+    private userService: UserService,
+    private appComponent: AppComponent,
+    private catalogService: CatalogService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-
+    this.userService
+      .getProfile(this.appComponent.sessionStorage)
+      .subscribe((data: any) => {
+        this.catalogService
+          .getOwnProducts(data._id)
+          .subscribe((data: any) => {
+            if (data?.message) {
+              this.errors = data
+            } else {
+              this.products = data;
+            }
+          });
+      });
   }
 
+  onSelect(id: string) {
+    this.router.navigate(['/catalog/details/', id])
+  }
 }
