a14162a1caa78cc998353475b3f947d1dbeae41b
Емил Стойчев <estallinone@gmail.com>
2022-12-17 13:15:29 +0200

    Make search to work and set styles

--- my-app/src/app/main/catalog/catalog.component.ts
+++ my-app/src/app/main/catalog/catalog.component.ts
@@ -8,30 +8,63 @@ import { CatalogService } from 'src/app/services/catalog/catalog.service';
   styleUrls: ['./catalog.component.css'],
 })
 export class CatalogComponent implements OnInit {
-
   public products = [] as any;
-  public togglePrice: boolean = false
+  public defaultProducts = [] as any;
+  public searchProducts = [] as any;
+  public toggleSort: boolean = false;
 
   constructor(private catalogService: CatalogService, private router: Router) {}
 
   ngOnInit() {
-    this.catalogService.getProducts().subscribe(data => this.products = data);
+    this.catalogService
+      .getProducts()
+      .subscribe(
+        (data) => ((this.products = data), (this.defaultProducts = data))
+      );
   }
 
   onSelect(id: string) {
-    this.router.navigate(['/catalog/details/', id])
+    this.router.navigate(['/catalog/details/', id]);
+  }
+
+  searchHandler(e: any) {
+    let text = e.currentTarget.parentElement.childNodes[0];
+
+    if (text.value.trim() != '' && text.value.length > 0 && text.value != '') {
+      this.products = this.defaultProducts;
+
+      let cnt = this.products.filter((x: any) => {
+        return x.title.toLowerCase().startsWith(text.value.toLowerCase());
+      });
+
+      if (cnt?.length == 0 && text.style.color != 'red') {
+        text.style.color = 'red';
+
+        setTimeout(() => {
+          text.style.color = 'black';
+        }, 1500);
+      }
+
+      this.products = cnt;
+    } else {
+      text.style.color = 'black';
+      this.products = this.defaultProducts;
+    }
   }
 
   sortBy(word: string) {
-    this.togglePrice = !this.togglePrice
+    this.toggleSort = !this.toggleSort;
 
     this.products = this.products.sort((a: any, b: any) => {
-      if(this.togglePrice) {
-        return word == 'likes'? a?.[word]?.length - b?.[word]?.length : a?.[word] - b?.[word]
+      if (this.toggleSort) {
+        return word == 'likes'
+          ? a?.[word]?.length - b?.[word]?.length
+          : a?.[word] - b?.[word];
       } else {
-        return word == 'likes'? b?.[word]?.length - a?.[word]?.length : b?.[word] - a?.[word]
+        return word == 'likes'
+          ? b?.[word]?.length - a?.[word]?.length
+          : b?.[word] - a?.[word];
       }
-    })
+    });
   }
-
 }
